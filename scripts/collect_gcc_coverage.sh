b5bfdf0159378d6d3d3a5d7e148b873b6c311c90
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2020-11-14 03:49:02 +0900

    chore: include src directory for coverage measurement

--- scripts/collect_gcc_coverage.sh
+++ scripts/collect_gcc_coverage.sh
@@ -10,6 +10,6 @@ HTML=$DIR/coverage/html
 ROOT=$(realpath $(dirname $(dirname $0)))
 
 lcov --rc lcov_branch_coverage=1 --directory ./ --capture --output-file $COV
-lcov --rc lcov_branch_coverage=1 --extract $COV "${ROOT}/include/*" --output-file $COV
+lcov --rc lcov_branch_coverage=1 --extract $COV "${ROOT}/include/*" "${ROOT}/src/*" --output-file $COV
 genhtml --rc lcov_branch_coverage=1 --output-directory $HTML $COV
 lcov --rc lcov_branch_coverage=1 --list $COV
