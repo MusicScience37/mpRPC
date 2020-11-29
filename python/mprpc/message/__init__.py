1842e93f5b15fececf97b6cffcdb101ed4ab6595
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2020-11-29 22:47:08 +0900

    feat: validation of data

--- python/mprpc/message/__init__.py
+++ python/mprpc/message/__init__.py
@@ -8,6 +8,7 @@ from ._msgtype import MsgType
 from ._msgid import MAX_MSGID, is_valid_msgid
 from ._message import Request, Response, Notification
 from ._message_validation import validate_message
+from ._data_validation import validate_data, validate_data_list
 
 for obj in (
     MessageData,
@@ -17,6 +18,8 @@ for obj in (
     Notification,
     validate_message,
     is_valid_msgid,
+    validate_data,
+    validate_data_list,
 ):
     obj.__module__ = "mprpc.message"
 del obj
