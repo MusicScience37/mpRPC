41fed5ebc0f6dc58c0fab7d11bf9e7ab4ad12bc4
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2020-12-12 04:56:09 +0900

    feat: add type_caster of shared_function for use in server implementation

--- python/test/mprpc_cpp_test/src/bind.cpp
+++ python/test/mprpc_cpp_test/src/bind.cpp
@@ -20,10 +20,13 @@
 #include <pybind11/pybind11.h>
 
 #include "mock_session.h"
+#include "mprpc/shared_function_caster.h"
 #include "mprpc/transport/mock/mock_address.h"
 
 PYBIND11_MODULE(mprpc_cpp_test, module) {
     module.doc() = "wrapper of C++ classes for tests of mpRPC";
+
+    pybind11::module_::import("_mprpc_cpp");
 
     using mprpc::transport::address;
     using mprpc::transport::mock::mock_address;
@@ -36,4 +39,8 @@ PYBIND11_MODULE(mprpc_cpp_test, module) {
     pybind11::class_<python_mock_session, session,
         std::shared_ptr<python_mock_session>>(module, "MockSession")
         .def(pybind11::init<>());
+
+    module.def("call_int_adder",
+        [](const stl_ext::shared_function<int(int, int)>& callback, int x,
+            int y) { return callback(x, y); });
 }
