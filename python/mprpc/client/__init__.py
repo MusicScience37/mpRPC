c70b780e49b06e63281543ca8efa8e173f8ba571
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-01-10 12:07:25 +0900

    feat: add a client class

--- python/mprpc/client/__init__.py
+++ python/mprpc/client/__init__.py
@@ -4,9 +4,11 @@
 from .._mprpc_cpp import (
     PythonClientHelper,
 )
+from ._client import Client
 
 for obj in (
     PythonClientHelper,
+    Client,
 ):
     obj.__module__ = "mprpc.client"
 del obj
